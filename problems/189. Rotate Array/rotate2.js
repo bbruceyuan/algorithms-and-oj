c6ee2d1148b7d4225542f044b7d593767d48e78e
BBruceyuan <bruceyuan123@163.com>
2016-11-24 23:14:24 +0800

    Update rotate2.js

--- problems/189. Rotate Array/rotate2.js
+++ problems/189. Rotate Array/rotate2.js
@@ -3,7 +3,25 @@
 //  * @param {number} k
 //  * @return {void} Do not return anything, modify nums in-place instead.
 //  */
+/*
+直接看别人的思路
+For each element at position i, we need to move them to (i + k) % n, use a buffer to save the element that has been replaced, and move the replaced element to its right place.
+We will start from an element, and mark that, if we come back to the starting point, means we need to start over from the element next of the current starting point.
+Work terminates when we have move exactly n times.
 
+Example:
+Input:
+[1,2,3,4,5,6]
+2
+
+Process:
+
+startIdx = 0
+1, [1,2,3,4,5,6] -> 3, [1,2,1,4,5,6] -> 5, [1,2,1,4,3,6] -> [5,2,1,4,3,6]
+
+startIdx = 1
+2, [5,2,1,4,3,6] -> 4, [5,2,1,2,3,6] -> 6, [5,2,1,2,3,4] -> [5,6,1,2,3,4]
+*/
 var rotate = function(nums, k){
 	var startIndex = 0,
 		len = nums.length,
