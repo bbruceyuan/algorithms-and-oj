51c1d4a254a795356a7882d582a521f6a675e036
BBruceyuan <bruceyuan123@163.com>
2017-02-12 09:34:51 +0800

    Add files via upload

---
+++ problems/100. Same Tree/isSameTree.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {boolean}
+ */
+var isSameTree = function(p, q) {
+    if (p === null && q === null) return true;
+    if (p === null || q === null) return false;
+    if (p.val !== q.val) {
+        return false;
+    } else {
+     return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+};
